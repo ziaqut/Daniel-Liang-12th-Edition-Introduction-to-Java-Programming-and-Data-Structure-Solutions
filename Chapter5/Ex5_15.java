7786b40ea4173aba40f5fa8cf85336ff6182f817
ZuebeyirEser <zubeyir.eser@icloud.com>
2023-12-29 15:41:20 +0100

    Added 5.12 and 5.18 solutions. Also added SentinelValue

--- Chapter5/Ex5_15.java
+++ Chapter5/Ex5_15.java
@@ -3,7 +3,6 @@ package Chapter5;
 public class Ex5_15 {
     public static void main(String[] args) {
         int n = 7;
-        int s = 1;
         for (int i = 7; i > 0 ; i--) {
             for (int j = 1; j<i ;j++) {
                 System.out.print(" ");
