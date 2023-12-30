47ad71b6edb483533f018dde93f0cb98d1c57a8f
ZuebeyirEser <zubeyir.eser@icloud.com>
2023-12-30 15:26:55 +0100

    Added 5.17

--- Chapter5/Ex5_17.java
+++ Chapter5/Ex5_17.java
@@ -6,20 +6,20 @@ public class Ex5_17 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the number of lines: ");
-        int numberOfLine = input.nextInt();
+        int rows = input.nextInt();
 
-        for (int i = 1; i <= numberOfLine; i++) {
-            for (int j = numberOfLine - i; j > 0; j--) {
+        for (int i = 1; i <= rows; i++) {
+            for (int k = rows - i; k > 0; k--) {
                 System.out.print("  ");
             }
-
-            for (int k = i; k >= 1; k--) {
-                System.out.print(k + " ");
+            for (int j = i;j >= 1 ; j--) {
+                System.out.print(j + " ");
             }
-            for (int l = 2; l <= i; l++) {
-                System.out.print(l + " ");
+            for (int s = 2; s <= i;s++) {
+                System.out.print(s + " ");
             }
             System.out.println();
         }
+
     }
 }
