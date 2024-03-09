76a015089a50b3187a1b46915bae45651af9cb6c
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-03-09 17:16:29 +0100

    forgot to print prime number with line 10 prime

--- Chapter5/Ex5_20.java
+++ Chapter5/Ex5_20.java
@@ -3,9 +3,15 @@ package Chapter5;
 public class Ex5_20 {
     // display prime number 2 to 1000
     public static void main(String[] args) {
+        int count = 0;
         for (int i = 2; i <= 1000; i++) {
             if (isPrime(i)) {
-                System.out.printf("%-10d", i);
+                System.out.print(i + " ");
+                count++;
+                if (count % 10 == 0) {
+                    System.out.println();
+                    count = 0;
+                }
             }
         }
     }
