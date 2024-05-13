dea9dedf9e7a11130583f4635b7d898a81afb71d
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-05-13 10:49:31 +0200

    added solution of 5.23 and 5.24

--- Chapter5/Ex5_24.java
+++ Chapter5/Ex5_24.java
@@ -1,10 +1,10 @@
 package Chapter5;
 
-public class Ex5_23 {
+public class Ex5_24 {
     public static void main(String[] args) {
         double sum = 0.0D;
-        for (int i = 50000; i > 0; i--) {
-            sum += 1.0 / i;
+        for (int i = 97; i>0; i =- 2) {
+            sum += (double) i / (i + 2);
         }
         System.out.println(sum);
     }
