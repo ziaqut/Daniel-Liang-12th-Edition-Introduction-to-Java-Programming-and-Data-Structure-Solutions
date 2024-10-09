44968dae3190b7257154a64d189915b00e6d35b1
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-10-09 11:17:01 +0200

    fixed the wrong problem solution

--- Chapter6/Ex6_12.java
+++ Chapter6/Ex6_12.java
@@ -1,28 +1,21 @@
 package Chapter6;
 
 public class Ex6_12 {
-
-    public static double computeCommission(double salesAmount) {
-        double commission = 0.0D;
-
-        // Commission rate
-        if (salesAmount <= 10000) {
-            commission = salesAmount * 0.09;
-        } else if (salesAmount <= 15000) {
-            commission = 10000 * 0.09 + (salesAmount - 10000) * 0.12;
-        } else {
-            commission = 10000 * 0.09 + 5000 * 0.12 + (salesAmount - 15000) * 0.15;
+    public static void printChars(char ch1, char ch2, int numberPerLine) {
+        int count = 0;
+        for (char ch = ch1; ch <= ch2; ch++) {
+            System.out.print(ch + " ");
+            count++;
+            if (count % numberPerLine == 0) {
+                System.out.println();
+            }
         }
-
-        return commission;
+        if (count % numberPerLine != 0) {
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        System.out.printf("%-15s%-15s\n", "Sales Amount", "Commission");
-
-        for (double salesAmount = 10000; salesAmount <= 100000; salesAmount += 5000) {
-            double commission = computeCommission(salesAmount);
-            System.out.printf("%-15.0f%-15.2f\n", salesAmount, commission);
-        }
+        printChars('1', 'Z', 10);
     }
 }
